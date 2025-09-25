8968bca7d2effa7e1f4027a36ccfc391dccc05c5
valice12 <calvinaritamaga@gmail.com>
2025-09-25 15:41:26 +0700

    fix color in about us

--- my-porto/src/components/About.tsx
+++ my-porto/src/components/About.tsx
@@ -2,7 +2,7 @@ function About() {
   return (
     <section id="about" className="py-20 bg-gray-100 w-full">
       <div className="w-full h-screen mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6">About Me</h2>
+        <h2 className="text-3xl font-bold mb-6 text-black">About Me</h2>
         <p className="text-gray-700 leading-relaxed">
           Saya seorang mahasiswa dan web developer yang suka membangun aplikasi
           dengan React dan TypeScript. Saya memiliki kemampuan dibidang front-end, back-end, dan juga pengolahan data.
