9fad7dd6c4369a46538995036dc5b25bf2090a35
valice12 <calvinaritamaga@gmail.com>
2025-09-25 20:13:26 +0700

    add photo
    and some changes

--- my-porto/src/components/Contact.tsx
+++ my-porto/src/components/Contact.tsx
@@ -2,7 +2,7 @@ function Contact() {
   return (
     <section id="contact" className="py-20 bg-gray-100 w-full">
       <div className="max-w-4xl mx-auto px-6 text-center">
-        <h2 className="text-3xl font-bold mb-6 text-black">Contact</h2>
+        <h2 className="text-3xl font-bold mb-6">Contact</h2>
         <p className="text-gray-700">Hubungi saya di:</p>
         <div className="mt-4 space-y-2">
           <a href="mailto:calvinaritamaga@gmail.com" className="text-blue-600 block">📧 calvinaritamaga@gmail.com</a>
--- my-porto/src/components/Hero.tsx
+++ my-porto/src/components/Hero.tsx
@@ -2,11 +2,11 @@ function Hero() {
   return (
     <section className="w-full h-screen flex flex-col justify-center items-center bg-gradient-to-r from-blue-500 to-purple-500">
       <div className="flex flex-col justify-center items-center h-screen text-center">
-        <div className="w-32 h-32 border-2 border-white rounded-full flex items-center justify-center text-black">
-          Profile
+        <div className="w-32 h-32 border-2 border-white rounded-full flex items-center justify-center">
+          <img src="/Profile.jpg" alt="Fot Profile" className="w-full h-full object-cover rounded-full"/>
         </div>
         <h1 className="mt-4 text-2xl font-bold">Calvin Aritama</h1>
-        <p className="text-sm">Web Developer | React Enthusiast</p>
+        <p className="text-sm">Full-Stack Minded | Data Entusiast</p>
         <button className="mt-4 px-4 py-2 bg-white text-blue-600 rounded">Contact Me</button>
       </div>
     </section>
